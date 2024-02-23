855f6ef6a986695c4051462d68355f4614d4bd96
SnnBrgmv <sananai@code.edu.az>
2024-02-24 00:26:58 +0400

    GroupChatApp with SignalR

---
+++ SignalRChat/SignalRChat/Controllers/ChatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalRChat.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SignalRChat/SignalRChat/wwwroot/js/main.js
@@ -0,0 +1,36 @@
+﻿let connection = new signalR.HubConnectionBuilder().withUrl("/chathub").build();
+let send = document.querySelector(".send");
+
+connection.start().then(function () {
+    console.log("SignalR Connected");
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+connection.on("ReceiveMessage", function (user, message) {
+    var encodedMessage = user + ": " + message;
+    var li = document.createElement("li");
+    li.textContent = encodedMessage;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+function sendMessage() {
+    var user = document.getElementById("userInput").value;
+    var messageInput = document.getElementById("messageInput");
+
+    var message = messageInput.value.trim();
+
+    if (!message) {
+        console.error("Message is empty or contains only whitespaces");
+        return;
+    }
+
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        console.error(err.toString());
+    });
+
+
+    messageInput.value = '';
+}
+
+send.addEventListener("click", sendMessage)
